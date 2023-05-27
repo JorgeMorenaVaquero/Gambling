81af7190a1b24912f762ee0b4e20f500faca76eb
DavidCF <cordoba.davidcf@gmail.com>
2023-05-27 12:05:14 +0200

    ñadida clase SorteoEuromillones

---
+++ gambling/src/main/java/com/daw1/gambling/SorteoEuromillones.java
@@ -0,0 +1,54 @@
+package com.daw1.gambling;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class SorteoEuromillones extends Sorteo {
+
+	private int[] combinacion;
+	private int[] estrellas;
+
+	/**
+	 * Constructor de un sorteo de euromillones con resultado
+	 * 
+	 * @param id
+	 * @param fechaApertura
+	 * @param fechaCierre
+	 * @param fechaHora
+	 * @param resultado
+	 * @param combinacion
+	 * @param estrellas
+	 */
+	public SorteoEuromillones(long id, Date fechaApertura, Date fechaCierre, Timestamp fechaHora, String resultado,
+			int[] combinacion, int[] estrellas) {
+		super(id, fechaApertura, fechaCierre, fechaHora, resultado);
+		this.tipo = TipoSorteo.EUROMILLONES;
+		this.combinacion = combinacion;
+		this.estrellas = estrellas;
+	}
+
+	/**
+	 * Constructor de un sorteo de euromillones sin resultado
+	 * 
+	 * @param id
+	 * @param fechaApertura
+	 * @param fechaCierre
+	 * @param fechaHora
+	 * @param resultado
+	 * @param combinacion
+	 * @param estrellas
+	 */
+	public SorteoEuromillones(long id, Date fechaApertura, Date fechaCierre, Timestamp fechaHora, int[] combinacion,
+			int[] estrellas) {
+		super(id, fechaApertura, fechaCierre, fechaHora);
+		this.tipo = TipoSorteo.EUROMILLONES;
+		this.combinacion = combinacion;
+		this.estrellas = estrellas;
+	}
+
+	@Override
+	public void generarResultado() {
+		// TODO Auto-generated method stub
+
+	}
+}
