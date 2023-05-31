72f1212644d27ac8704d894d9e42fc97ce084872
JorgeDLMV <jormoreva@gmail.com>
2023-05-31 13:46:57 +0200

    Añadido constructor al enumerado.

--- gambling/src/main/java/com/daw1/gambling/TipoSorteo.java
+++ gambling/src/main/java/com/daw1/gambling/TipoSorteo.java
@@ -1,9 +1,26 @@
 package com.daw1.gambling;
 
 public enum TipoSorteo {
-	LOTERIA_NACIONAL,
-	QUINIELA,
-	EUROMILLONES,
-	PRIMITIVA,
-	GORDO
+	LOTERIA_NACIONAL("LOTERIA_NACIONAL", ResultadoLoteriaNacional.class),
+	QUINIELA("QUINIELA", ResultadoQuiniela.class),
+	EUROMILLONES("EUROMILLONES", ResultadoEuromillones.class),
+	PRIMITIVA("PRIMITIVA", ResultadoPrimitiva.class),
+	GORDO("GORDO", ResultadoGordo.class);
+	
+	private String value;
+	private Class<?> clase;
+
+	private TipoSorteo(String value, Class<?> clase) {
+		this.value = value;
+		this.clase = clase;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public Class<?> getClase() {
+		return clase;
+	}
+
 }
