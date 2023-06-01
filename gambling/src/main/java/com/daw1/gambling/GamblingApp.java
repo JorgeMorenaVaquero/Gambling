17f327e9857b8beca9abc2024d69a1cc16ed7597
DavidCF <cordoba.davidcf@gmail.com>
2023-06-01 11:30:53 +0200

    Añadida funcionalidad en seleccionarOpcionNoRegistrado().

--- gambling/src/main/java/com/daw1/gambling/GamblingApp.java
+++ gambling/src/main/java/com/daw1/gambling/GamblingApp.java
@@ -1,15 +1,33 @@
 package com.daw1.gambling;
 
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
 import java.util.Scanner;
+
+import com.daw1.gambling.controller.ApuestaController;
+import com.daw1.gambling.controller.JugadorController;
+import com.daw1.gambling.controller.SorteoController;
+import com.daw1.gambling.service.AutentificacionService;
 
 public class GamblingApp {
 
 	private Scanner teclado;
+
+	private JugadorController jugadorController;
+	private SorteoController sorteoController;
+	private ApuestaController apuestaController;
+
+	private AutentificacionService service;
+
 	private Jugador jugador;
 
 	public GamblingApp() {
 		super();
 		this.teclado = new Scanner(System.in);
+		this.jugadorController = new JugadorController();
+		this.sorteoController = new SorteoController();
+		this.apuestaController = new ApuestaController();
 	}
 
 	private void mostrarMenuNoRegistrado() {
@@ -28,13 +46,39 @@ public class GamblingApp {
 			mostrarMenuRegistrado();
 		}
 	}
-	
+
 	private void seleccionarOpcionNoRegistrado(int opcion) {
-		
+		System.out.println("Introduzca los siguientes datos para registrarse:");
+
+		System.out.println("DNI\n");
+		String dni = teclado.nextLine();
+
+		System.out.println("Telefono\n");
+		String telefono = teclado.nextLine();
+
+		System.out.println("Correo electronico\n");
+		String correoElectronico = teclado.nextLine();
+
+		System.out.println("Contraseña\n");
+		String contrasenna = teclado.nextLine();
+
+		System.out.println("Saldo inicial\n");
+		BigDecimal dinero = new BigDecimal(Integer.parseInt(teclado.nextLine()));
+
+		jugador = new Jugador(dni, dinero, correoElectronico, contrasenna, telefono);
+
+		try {
+			if (service.puedeRegistrarse(jugador)) {
+				jugadorController.insertJugador(jugador);
+			}
+		} catch (ClassNotFoundException | SQLException | IOException e) {
+			e.printStackTrace();
+		}
+
 	}
-	
+
 	private void seleccionarOpcionRegistrado(int opcion) {
-		
+
 	}
 
 	private void seleccionarOpcion(int opcion) {
@@ -54,7 +98,7 @@ public class GamblingApp {
 		while (!finalizado) {
 			mostrarMenu();
 			System.out.println("Introduce -1 para finalizar");
-			
+
 			int opcion = Integer.parseInt(teclado.nextLine());
 			if (opcion == -1) {
 				finalizado = true;
