3f68d8ce1bc9a106e97779076cfe5a04e391655c
DavidCF <cordoba.davidcf@gmail.com>
2023-06-01 13:32:34 +0200

    Añadida funcionalidad de generarResultadoApuesta en SorteoEruomillones.

--- gambling/src/main/java/com/daw1/gambling/sorteo/SorteoEuromillones.java
+++ gambling/src/main/java/com/daw1/gambling/sorteo/SorteoEuromillones.java
@@ -10,6 +10,9 @@ import com.daw1.gambling.resultado.ResultadoEuromillones;
 
 public class SorteoEuromillones extends Sorteo {
 
+	private static final int COMBINACION = 6;
+	private static final int ESTRELLAS = 2;
+	
 	/**
 	 * Constructor de un sorteo de euromillones con resultado
 	 * 
@@ -46,10 +49,61 @@ public class SorteoEuromillones extends Sorteo {
 
 	@Override
 	protected Resultado generarResultadoApuesta(Scanner teclado) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		int[] combinacion = new int[COMBINACION];
+		int[] estrellas = new int[ESTRELLAS];
+		
+		System.out.println("Introduzca una combinacion de seis numeros entre 1 y 50 cada uno: ");
+		
+		for (int i = 0; i < COMBINACION; i++) {
+			System.out.print("Numero "+(i+1)+": ");
+			combinacion[i] = numeroCorrectoCombinacion(teclado);
+		}
+		
+		System.out.println("Introduzca una combinacion de dos numeros de estrellas entre 1 y 12 cada uno: ");
+		
+		for (int i = 0; i < ESTRELLAS; i++) {
+			System.out.print("Numero "+(i+1)+": ");
+			estrellas[i] = numeroCorrectoEstrellas(teclado);
+		}
+		
+		ResultadoEuromillones euromillones = new ResultadoEuromillones(combinacion, estrellas);
+		
+		return euromillones;
 	}
-
+	
+	private int numeroCorrectoCombinacion(Scanner teclado) {
+		boolean correcto = false;
+		int numero = Integer.parseInt(teclado.nextLine());
+		
+		while(!correcto) {
+			
+			if (numero < 1 || numero > 50) {
+				numero = Integer.parseInt(teclado.nextLine());
+			} else {
+				correcto = true;
+			}
+		}
+		
+		return numero;
+	}
+	
+	private int numeroCorrectoEstrellas(Scanner teclado) {
+		boolean correcto = false;
+		int numero = Integer.parseInt(teclado.nextLine());
+		
+		while(!correcto) {
+			
+			if (numero < 1 || numero > 12) {
+				numero = Integer.parseInt(teclado.nextLine());
+			} else {
+				correcto = true;
+			}
+		}
+		
+		return numero;
+	}
+	
 	@Override
 	public String toString() {
 		return "SorteoEuromillones [" + super.toString() + "]";
